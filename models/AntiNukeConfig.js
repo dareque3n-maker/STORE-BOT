438bdb542dac0ed78755c86fbaf0055b6c8b61c9
dareque3n-maker <dareque3n@gmail.com>
2026-07-23 11:53:34 +0530

    Update AntiNukeConfig.js

--- models/AntiNukeConfig.js
+++ models/AntiNukeConfig.js
@@ -1,19 +1,10 @@
 const mongoose = require('mongoose');
 
-const AntiNukeSchema = new mongoose.Schema({
+const antiNukeConfigSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
-    enabled: { type: Boolean, default: true },
     logChannelId: { type: String, default: null },
-    whitelistedUsers: { type: [String], default: [] },
-    
-    // Emergency Triggered Snapshot & Transcript (Auto-delete after 10 mins)
-    emergencyBackups: [{
-        backupId: String,
-        createdAt: { type: Date, default: Date.now, expires: 600 }, // 600 seconds = 10 minutes
-        deletedChannelName: String,
-        transcript: String,
-        channelData: Object
-    }]
+    whitelistedBots: { type: [String], default: [] },
+    whitelistedUsers: { type: [String], default: [] }
 });
 
-module.exports = mongoose.model('AntiNukeConfig', AntiNukeSchema);
+module.exports = mongoose.model('AntiNukeConfig', antiNukeConfigSchema);
